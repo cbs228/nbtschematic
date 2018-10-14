b9f8f0dd4f21bdaf055d9f8aeabf426f15335625
Colin S <3526918+cbs228@users.noreply.github.com>
2018-10-13 21:59:38 -0500

    Add Sphinx support
    Documentation-only changes have been made to the source code.

---
+++ docs/source/index.rst
@@ -0,0 +1,32 @@
+.. toctree::
+   :maxdepth: 2
+
+.. mdinclude:: ../../README.md
+
+Module Reference
+==================
+
+Schematic Files
+---------------
+
+.. autoclass:: nbtschematic.SchematicFile
+   :members:
+   :inherited-members:
+
+Schematic Components
+--------------------
+
+.. autoclass:: nbtschematic.Schematic
+   :members:
+
+Entities
+~~~~~~~~
+
+.. autoclass:: nbtschematic.Entity
+   :members:
+
+Block Entities
+~~~~~~~~~~~~~~
+
+.. autoclass:: nbtschematic.BlockEntity
+   :members:
--- nbtschematic/schematic.py
+++ nbtschematic/schematic.py
@@ -4,49 +4,69 @@ import numpy as np
 import nbtlib as nbt
 
 
-Entity = nbt.schema('Entity', {
-    'id': nbt.String,
-    'Pos': nbt.List[nbt.Double],
-    'Motion': nbt.List[nbt.Double],
-    'Rotation': nbt.List[nbt.Float],
-    'FallDistance': nbt.Float,
-    'Fire': nbt.Short,
-    'Air': nbt.Short,
-    'OnGround': nbt.Byte,
-    'NoGravity': nbt.Byte,
-    'Invulnerable': nbt.Byte,
-    'PortalCooldown': nbt.Int,
-    'UUIDMost': nbt.Long,
-    'UUIDLeast': nbt.Long
-})
+class Entity(nbt.CompoundSchema):
+    """
+    Entities describe objects which are not anchored to blocks, like mobs.
+    """
+    schema = {
+        'id': nbt.String,
+        'Pos': nbt.List[nbt.Double],
+        'Motion': nbt.List[nbt.Double],
+        'Rotation': nbt.List[nbt.Float],
+        'FallDistance': nbt.Float,
+        'Fire': nbt.Short,
+        'Air': nbt.Short,
+        'OnGround': nbt.Byte,
+        'NoGravity': nbt.Byte,
+        'Invulnerable': nbt.Byte,
+        'PortalCooldown': nbt.Int,
+        'UUIDMost': nbt.Long,
+        'UUIDLeast': nbt.Long
+    }
 
-BlockEntity = nbt.schema('BlockEntity', {
-    'id': nbt.String,
-    'x': nbt.Int,
-    'y': nbt.Int,
-    'z': nbt.Int
-})
 
-Schematic = nbt.schema('Schematic', {
-    'Height': nbt.Short,
-    'Length': nbt.Short,
-    'Width': nbt.Short,
-    'Materials': nbt.String,
-    # 8 bits per block. Sorted by height (bottom to top) then length then width
-    # -- the index of the block at X,Y,Z is Y×length×width + Z×width + X.
-    'Blocks': nbt.ByteArray,
-    'Data': nbt.ByteArray,
-    'Entities': nbt.List[Entity],
-    'TileEntities': nbt.List[BlockEntity]
-})
+class BlockEntity(nbt.CompoundSchema):
+    """
+    Block entities contain additional metadata for placed blocks
+    """
+    schema = {
+        'id': nbt.String,
+        'x': nbt.Int,
+        'y': nbt.Int,
+        'z': nbt.Int
+    }
 
-""" Indicates schematic from a Classic world """
+
+class Schematic(nbt.CompoundSchema):
+    """
+    Schematic files represent a small section of a level
+
+    Key fields include:
+
+    - `Blocks`: A dense array of block IDs at each coordinate. Sorted by block
+      height (bottop to top), then length (``Z``), then width (``X``). The
+      index of the block at
+      ``blocks[X, Y, Z]`` is ``Y * length * width + Z * width + X.``
+    - `Data`: A dense array of data values for each block. This field shares
+      sizes and indexing with `Blocks`.
+    - `Entities`: A list of Compound tags which are entities.
+    - `TileEntities`: A list of Compound tags which are block entities, which
+      were previously known as tile entities.
+    """
+    schema = {
+        'Height': nbt.Short,
+        'Length': nbt.Short,
+        'Width': nbt.Short,
+        'Materials': nbt.String,
+        'Blocks': nbt.ByteArray,
+        'Data': nbt.ByteArray,
+        'Entities': nbt.List[Entity],
+        'TileEntities': nbt.List[BlockEntity]
+    }
+
+
 MATERIAL_CLASSIC = "Classic"
-
-""" Indicates schematic from a Pocket world """
 MATERIAL_POCKET = "Pocket"
-
-""" Indicates schematic from Alpha and newer levels (Default) """
 MATERIAL_ALPHA = "Alpha"
 
 
@@ -89,7 +109,7 @@ class SchematicFile(nbt.File, Schematic):
         Resizing the schematic clears the blocks and data
 
         :param shape: New dimensions for the schematic, as a tuple of
-        ``(n_y, n_z, n_z)``.
+               ``(n_y, n_z, n_x)``.
         """
 
         self.root['Height'] = nbt.Short(shape[0])
@@ -100,14 +120,28 @@ class SchematicFile(nbt.File, Schematic):
 
     @classmethod
     def load(cls, filename, gzipped=True, byteorder='big') -> 'SchematicFile':
+        """
+        Load a schematic file from disk
+
+        If the schematic file is already loaded into memory, use the
+        :meth:`~from_buffer()` method instead.
+
+        :param filename: Path to a schematic file on disk.
+        :param gzipped: Schematic files are always stored gzipped. This option
+               defaults to True
+        :param byteorder: Schematic files are always stored in big endian
+               number format.
+        :return: Loaded schematic
+        """
         return super().load(filename=filename,
                             gzipped=gzipped, byteorder=byteorder)
 
     @property
     def shape(self) -> Tuple[nbt.Short, nbt.Short, nbt.Short]:
         """ Schematic shape
+
         :return: Shape of the schematic, as a tuple of ``Y``, ``Z``, and ``X``
-        size.
+                 size.
         """
         return self.root['Height'], self.root['Length'], self.root['Width']
 
@@ -120,7 +154,7 @@ class SchematicFile(nbt.File, Schematic):
         coerce it to a pure numpy array with ``numpy.asarray()``
 
         :return: 3D array which contains a view into the block IDs.
-        Array indices are in ``Y``,``Z``,``X`` order.
+                 Array indices are in ``Y``, ``Z``, ``X`` order.
         """
         return self.root['Blocks'].reshape(self.shape, order='C').view()
 
@@ -142,7 +176,7 @@ class SchematicFile(nbt.File, Schematic):
         coerce it to a pure numpy array with ``numpy.asarray()``
 
         :return: 3D array which contains a view into the block data.
-        Array indices are in ``Y``,``Z``,``X`` order.
+                 Array indices are in ``Y``, ``Z``, ``X`` order.
         """
         return self.root['Data'].reshape(self.shape, order='C').view()
 
